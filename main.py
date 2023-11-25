32ef32b188e122a9e91aae86b937361b89d503ef
Himanshu <himanshupandey1036@gmail.com>
2023-11-25 10:30:55 +0530

    uncommenting main.py content

--- main.py
+++ main.py
@@ -5,49 +5,49 @@ from plant_care.pipeline.stage03_prepare_callback import PrepareCallbackPipeline
 from plant_care.pipeline.stage04_training import TrainingPipeline
 from plant_care.pipeline.stage05_model_python import ModelEvaluationPipeline
 
-# STAGE_NAME = "Data Ingestion"
-# try:
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
-#     obj = DataIngestionPipeline()
-#     obj.main()
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
-#     logger.info("x=============x")
-# except Exception as e:
-#     logger.error(e)
-#     raise e
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
+    logger.info("x=============x")
+except Exception as e:
+    logger.error(e)
+    raise e
 
-# STAGE_NAME = "Preapare Base Model"
-# try:
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
-#     obj = PrepareBaseModelPipeline()
-#     obj.main()
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
-#     logger.info("x=============x")
-# except Exception as e:
-#     logger.error(e)
-#     raise e
+STAGE_NAME = "Preapare Base Model"
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
+    obj = PrepareBaseModelPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
+    logger.info("x=============x")
+except Exception as e:
+    logger.error(e)
+    raise e
 
-# STAGE_NAME = "Preapare Callback"
-# try:
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
-#     obj = PrepareCallbackPipeline()
-#     obj.main()
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
-#     logger.info("x=============x")
-# except Exception as e:
-#     logger.error(e)
-#     raise e
+STAGE_NAME = "Preapare Callback"
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
+    obj = PrepareCallbackPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
+    logger.info("x=============x")
+except Exception as e:
+    logger.error(e)
+    raise e
 
-# STAGE_NAME = "Training Model"
-# try:
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
-#     obj = TrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
-#     logger.info("x=============x")
-# except Exception as e:
-#     logger.error(e)
-#     raise e
+STAGE_NAME = "Training Model"
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<<<<<<<< ")
+    obj = TrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Ended <<<<<<<<<<<<<< ")
+    logger.info("x=============x")
+except Exception as e:
+    logger.error(e)
+    raise e
 
 STAGE_NAME = "Model Evaluation"
 try:
